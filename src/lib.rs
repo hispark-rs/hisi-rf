a3415c2f016f24cd18bffea467462afc9c164bfe
sanchuanhehe <wyihe5220@gmail.com>
2026-07-30 06:10:41 +0800

    feat: expose WS63 scheduler arena requirement

--- src/lib.rs
+++ src/lib.rs
@@ -91,8 +91,9 @@ pub mod ws63 {
         InitErrorKind, InstalledRadioArena, L2ProtocolDiagnostics, MissingCrypto, MissingPke,
         PkeNotRequired, PkeReady, RadioArena, RadioArenaStorage, ResourceReport, Resources,
         ResourcesBuilder, RfHeapMetrics, RxQueueDiagnostics, SELECTED_RF_ARENA_BYTES,
-        ScanDiagnostics, SelectedProfile, WifiDevice, WifiRxToken, WifiTxToken, WifiWpa2Smoltcp,
-        WifiWpa3Smoltcp, association_timing_diagnostics, blocking_backend_metrics, rf_heap_metrics,
+        SELECTED_TASK_STACK_ARENA_BYTES, ScanDiagnostics, SelectedProfile, WifiDevice, WifiRxToken,
+        WifiTxToken, WifiWpa2Smoltcp, WifiWpa3Smoltcp, association_timing_diagnostics,
+        blocking_backend_metrics, rf_heap_metrics,
     };
     #[cfg(feature = "ws63-station-pm-diagnostics")]
     #[doc(hidden)]
